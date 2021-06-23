e6f9764fe19bc19f6757ddec79d0247cf2fff081
yhirono <y.hirono.451@nitech.jp>
2021-06-23 18:53:11 +0900

    make svs_match

--- makedataset.py
+++ makedataset.py
@@ -1,6 +1,7 @@
 import csv
 import collections
 import numpy as np
+from numpy.core.machar import MachAr
 
 def ReadOriginalCSV(filename):
     csv_data = open(filename, 'r', newline='', encoding='utf-8')
@@ -41,6 +42,16 @@ def ReadOriginalCSV(filename):
     
     return np.array([header] + dataset)
 
+def MatchSVSlist(data):
+    svs_list =  np.loadtxt('./data/Kurume_img_list.csv', dtype='str', delimiter=',')
+    data_svs = [data[0]]
+    for d in data[1:]:
+        d_svs = [d for svs in svs_list if d[0] in svs[:11]]
+        if len(d_svs)>0:
+            data_svs.append(d_svs[0])
+    data_svs = np.array(data_svs)
+
+    return data_svs
 
 def WriteMultiCSV(data, filename):
     csv_file = open(filename, 'w', newline='')
@@ -72,16 +83,29 @@ def Add_Diseasae(dataset, file_label):
     name_list = dataset[1:,1]
     count = collections.Counter(name_list)
     count_sort = sorted(count.items(), key=lambda x:x[1], reverse=True)
-    # WriteDicCSV(count_sort, f'data/Disease_{file_label}_list.csv')
-    return dataset
+    return dataset, count_sort
 
     
 
 dataset = ReadOriginalCSV('data/ML180001-180660 _to_NIT.csv')
-# WriteSingleCSV(dataset[0][3:], 'data/Stain_list.csv')
+WriteSingleCSV(dataset[0][3:], 'data/Stain_list.csv')
 
-dataset_simple = Add_Diseasae(dataset, 'SimpleName')
-# WriteMultiCSV(dataset_simple, 'data/Data_SimpleName.csv')
+dataset_simple, count_sort = Add_Diseasae(dataset, 'SimpleName')
+WriteMultiCSV(dataset_simple, 'data/Data_SimpleName.csv')
+WriteDicCSV(count_sort, f'data/Disease_SimpleName_list.csv')
 
-dataset_full = Add_Diseasae(dataset, 'FullName')
-# WriteMultiCSV(dataset_full, 'data/Data_FullName.csv')
+dataset_full, count_sort = Add_Diseasae(dataset, 'FullName')
+WriteMultiCSV(dataset_full, 'data/Data_FullName.csv')
+WriteDicCSV(count_sort, f'data/Disease_FullName_list.csv')
+
+dataset_svs = MatchSVSlist(dataset)
+
+dataset_svs_simple, count_sort = Add_Diseasae(dataset_svs, 'SimpleName')
+WriteMultiCSV(dataset_svs_simple, 'data/Data_SimpleName_svs.csv')
+WriteDicCSV(count_sort, f'data/Disease_SimpleName_svs_list.csv')
+
+dataset_svs_full, count_sort = Add_Diseasae(dataset_svs, 'FullName')
+WriteMultiCSV(dataset_svs_full, 'data/Data_FullName_svs.csv')
+WriteDicCSV(count_sort, f'data/Disease_FullName_svs_list.csv')
+
+
